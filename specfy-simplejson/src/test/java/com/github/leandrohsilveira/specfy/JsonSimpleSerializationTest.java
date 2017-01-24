9810617307bdeab877bce3e472ba4aa2b2af5097
leandrohsilveira <leandro.hinckel@gmail.com>
2017-01-24 16:41:17 -0200

    JUnit test for JsonSimple serialization

--- specfy-simplejson/src/test/java/com/github/leandrohsilveira/specfy/JsonSimpleSerializationTest.java
+++ specfy-simplejson/src/test/java/com/github/leandrohsilveira/specfy/JsonSimpleSerializationTest.java
@@ -1,14 +1,10 @@
 package com.github.leandrohsilveira.specfy;
 
-import java.io.IOException;
-
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.junit.Test;
 
-import com.github.leandrohsilveira.specfy.exceptions.ClientException;
 import com.github.leandrohsilveira.specfy.exceptions.ClientSideValidationException;
-import com.github.leandrohsilveira.specfy.serializers.JSONObjectSerializer;
 import com.github.leandrohsilveira.specfy.sets.JsonSimpleSerializationSet;
 
 public class JsonSimpleSerializationTest {
@@ -16,19 +12,23 @@ public class JsonSimpleSerializationTest {
 	/**
 	 * Creating an API root with an Serialization Set defined. It apply for serialization and
 	 * deserialization of the {@link JSONObject} and {@link JSONArray} objects.
-	 * 
-	 * The isPOST method requires an class to specify what kind of object will be writen in request
-	 * body. The {@link JSONObjectSerializer} defined in {@link JsonSimpleSerializationSet} will set
-	 * the request content type to application/json.
 	 */
 	private static RESTfulClientSpec root = Spec.http().host("localhost").port(8080).api("api").register(new JsonSimpleSerializationSet());
 
-	private static ResourceActionSpec createUser = root.resource("users").isPOST(JSONObject.class);
-
 	@Test
-	public void hasSerializerTest() {
-		
-		createUser.newRequest()
-		
+	@SuppressWarnings("unchecked")
+	public void jsonSimpleBodyTest() throws Exception {
+		ResourceActionSpec createUser = root.resource("users").isPOST(JSONObject.class);
+		JSONObject user = new JSONObject();
+		user.put("username", "test");
+		user.put("password", "test123");
+		createUser.newRequest().body(user).validate();
+	}
+
+	@Test(expected = ClientSideValidationException.class)
+	@SuppressWarnings("unchecked")
+	public void jsonSimpleRequiredBodyNotBoundTest() throws Exception {
+		ResourceActionSpec createUser = root.resource("users").isPOST(JSONObject.class);
+		createUser.newRequest().validate();
 	}
 }
