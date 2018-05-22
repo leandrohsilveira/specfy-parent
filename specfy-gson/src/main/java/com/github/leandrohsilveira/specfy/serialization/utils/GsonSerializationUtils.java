950956f3b75a46182bcaebf76d52f7175b9c157a
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2018-05-22 16:46:54 -0300

    Format date utils

--- specfy-gson/src/main/java/com/github/leandrohsilveira/specfy/serialization/utils/GsonSerializationUtils.java
+++ specfy-gson/src/main/java/com/github/leandrohsilveira/specfy/serialization/utils/GsonSerializationUtils.java
@@ -1,13 +1,22 @@
 package com.github.leandrohsilveira.specfy.serialization.utils;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class GsonSerializationUtils {
 
+	private static final String DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
+
 	private GsonSerializationUtils() {
 	}
 
-	public static final Gson GSON = new GsonBuilder().setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").create();
+	public static final Gson GSON = new GsonBuilder().setDateFormat(DATE_FORMAT).create();
 
+	public static String formatDate(Date date) {
+		return new SimpleDateFormat(DATE_FORMAT).format(date);
+	}
+	
 }
